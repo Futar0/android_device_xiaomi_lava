44d9f2cb3ab8c8ca2916df609d739111926b57ec
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-10-06 20:04:44 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/evolution_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    evolution_lava-user \
+    evolution_lava-userdebug \
+    evolution_lava-eng
