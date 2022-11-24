f1c6152438e77adc3a4a981505d42ef6acf2d8d1
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-11-25 05:59:56 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/corvus_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    corvus_lava-user \
+    corvus_lava-userdebug \
+    corvus_lava-eng
